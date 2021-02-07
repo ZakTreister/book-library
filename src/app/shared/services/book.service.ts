0092bb84ff9304a1861cd4258a322bae784d171a
zakky <zakkyster@gmail.com>
2021-02-07 05:45:22 +0200

    Add FAVICON

--- src/app/shared/services/book.service.ts
+++ src/app/shared/services/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Book } from '../models/book';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class BookService {
   constructor(private http: HttpClient) { }
 
   public getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>('../../assets/books.json')
+    return this.http.get<Book[]>(`assets/books.json`)
   }
 }
